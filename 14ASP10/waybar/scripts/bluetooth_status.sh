efa6cd247af89369cbe164708fe2e90b6464f132
Dreed657 <ninjakiviWTF@gmail.com>
2025-10-28 13:35:53 +0200

    update bluetooth widget with more info

--- 14ASP10/waybar/scripts/bluetooth_status.sh
+++ 14ASP10/waybar/scripts/bluetooth_status.sh
@@ -1,6 +1,30 @@
-#!/bin/sh
-if bluetoothctl info | grep -q "Connected: yes"; then
-    echo "Connected"
-else
-    echo "Off"
+#!/bin/bash
+
+# Get all connected devices
+connected_devices=$(bluetoothctl info | grep "Device" | awk '{print $2}')
+
+if [ -z "$connected_devices" ]; then
+    echo " Off"
+    exit 0
 fi
+
+output=""
+while read -r mac; do
+    name=$(bluetoothctl info "$mac" | grep "Name:" | cut -d' ' -f2-)
+    battery_line=$(bluetoothctl info "$mac" | grep "Battery Percentage")
+
+    # Extract only the number in parentheses
+    if [[ $battery_line =~ \(([0-9]+)\) ]]; then
+        battery="${BASH_REMATCH[1]}%"
+    else
+        battery=""
+    fi
+
+    if [ -n "$battery" ]; then
+        output+="  ${name} (${battery})  "
+    else
+        output+="  ${name}  "
+    fi
+done <<< "$connected_devices"
+
+echo "$output"
